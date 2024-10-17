adf7a5837e50e8722564581066ee9eb09264af51
FekherJ <fekher.jarrar2@gmail.com>
2024-10-17 19:29:54 +0200

    complete tests added

--- contracts/ERC20Mock.sol
+++ contracts/ERC20Mock.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-    constructor() ERC20("ERC20Mock", "E20M") {}
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
     function mint(address account, uint256 amount) external {
         _mint(account, amount);
--- test/staking-test.js
+++ test/staking-test.js
@@ -10,7 +10,16 @@ describe("Staking Contract", function () {
         // Deploy ERC20 tokens
         const ERC20 = await ethers.getContractFactory("ERC20Mock");
         stakingToken = await ERC20.deploy("Staking Token", "STK");
+        await stakingToken.waitForDeployment(); // Wait for deployment to complete
+        const stakingTokenAddress = await stakingToken.getAddress(); // Get the staking token address
+
         rewardToken = await ERC20.deploy("Reward Token", "RWD");
+        await rewardToken.waitForDeployment(); // Wait for deployment to complete
+        const rewardTokenAddress = await rewardToken.getAddress(); // Get the reward token address
+
+        // DEBUG
+        console.log("Staking token deployed at:", stakingTokenAddress);
+        console.log("Reward token deployed at:", rewardTokenAddress);
 
         // Mint tokens to owner and users
         [owner, user1, user2] = await ethers.getSigners();
@@ -21,12 +30,16 @@ describe("Staking Contract", function () {
 
         // Deploy staking contract
         const Staking = await ethers.getContractFactory("Staking");
-        stakingContract = await Staking.deploy(stakingToken.address, rewardToken.address);
+        stakingContract = await Staking.deploy(stakingTokenAddress, rewardTokenAddress); // Pass the addresses correctly
+        await stakingContract.waitForDeployment(); // Wait for deployment to complete
+        const stakingContractAddress = await stakingContract.getAddress(); // Get the staking contract address
+
+        console.log("Staking Contract deployed at:", stakingContractAddress); // Debug log
     });
 
     it("Should allow user to stake tokens", async function () {
         // Approve and stake tokens for user1
-        await stakingToken.connect(user1).approve(stakingContract.address, ethers.parseEther("100"));
+        await stakingToken.connect(user1).approve(stakingContract.getAddress(), ethers.parseEther("100"));
         await stakingContract.connect(user1).stake(ethers.parseEther("100"));
 
         // Check staking balance
@@ -36,7 +49,7 @@ describe("Staking Contract", function () {
 
     it("Should allow user to withdraw staked tokens", async function () {
         // Stake first
-        await stakingToken.connect(user1).approve(stakingContract.address, ethers.parseEther("100"));
+        await stakingToken.connect(user1).approve(stakingContract.getAddress(), ethers.parseEther("100"));
         await stakingContract.connect(user1).stake(ethers.parseEther("100"));
 
         // Withdraw the tokens
@@ -49,7 +62,7 @@ describe("Staking Contract", function () {
 
     it("Should calculate and distribute rewards", async function () {
         // Stake tokens for user1
-        await stakingToken.connect(user1).approve(stakingContract.address, ethers.parseEther("100"));
+        await stakingToken.connect(user1).approve(stakingContract.getAddress(), ethers.parseEther("100"));
         await stakingContract.connect(user1).stake(ethers.parseEther("100"));
 
         // Fast-forward in time by 10 blocks
@@ -57,7 +70,7 @@ describe("Staking Contract", function () {
         await ethers.provider.send("evm_mine", []);
 
         // Owner transfers reward tokens to the contract for distribution
-        await rewardToken.transfer(stakingContract.address, ethers.parseEther("100"));
+        await rewardToken.transfer(stakingContract.getAddress(), ethers.parseEther("100"));
 
         // Check reward earned by user1
         const earned = await stakingContract.earned(user1.address);
@@ -66,7 +79,7 @@ describe("Staking Contract", function () {
 
     it("Should allow users to claim rewards", async function () {
         // Stake tokens for user1
-        await stakingToken.connect(user1).approve(stakingContract.address, ethers.parseEther("100"));
+        await stakingToken.connect(user1).approve(stakingContract.getAddress(), ethers.parseEther("100"));
         await stakingContract.connect(user1).stake(ethers.parseEther("100"));
 
         // Fast-forward in time by a few blocks
@@ -74,7 +87,7 @@ describe("Staking Contract", function () {
         await ethers.provider.send("evm_mine", []);
 
         // Owner transfers reward tokens to the contract
-        await rewardToken.transfer(stakingContract.address, ethers.parseEther("100"));
+        await rewardToken.transfer(stakingContract.getAddress(), ethers.parseEther("100"));
 
         // Claim the reward
         await stakingContract.connect(user1).getReward();
@@ -86,7 +99,7 @@ describe("Staking Contract", function () {
 
     it("Should allow users to compound rewards", async function () {
         // Stake tokens for user1
-        await stakingToken.connect(user1).approve(stakingContract.address, ethers.parseEther("100"));
+        await stakingToken.connect(user1).approve(stakingContract.getAddress(), ethers.parseEther("100"));
         await stakingContract.connect(user1).stake(ethers.parseEther("100"));
 
         // Fast-forward in time by a few blocks
@@ -94,14 +107,14 @@ describe("Staking Contract", function () {
         await ethers.provider.send("evm_mine", []);
 
         // Owner transfers reward tokens to the contract
-        await rewardToken.transfer(stakingContract.address, ethers.parseEther("100"));
+        await rewardToken.transfer(stakingContract.getAddress(), ethers.parseEther("100"));
 
         // Compound the rewards into the staking balance
         await stakingContract.connect(user1).compoundRewards();
 
         // Check updated staking balance (should include compounded rewards)
         const updatedBalance = await stakingContract.balances(user1.address);
-        expect(updatedBalance).to.be.above(ethers.utils.parseEther("100")); // Should be greater than initial staking amount
+        expect(updatedBalance).to.be.above(ethers.parseEther("100")); // Should be greater than initial staking amount
     });
 
     it("Should allow the owner to pause and unpause the contract", async function () {
@@ -109,7 +122,7 @@ describe("Staking Contract", function () {
         await stakingContract.pause();
 
         // Attempt staking while paused should fail
-        await stakingToken.connect(user1).approve(stakingContract.address, ethers.parseEther("100"));
+        await stakingToken.connect(user1).approve(stakingContract.getAddress(), ethers.parseEther("100"));
         await expect(stakingContract.connect(user1).stake(ethers.parseEther("100"))).to.be.revertedWith("Pausable: paused");
 
         // Unpause the contract
